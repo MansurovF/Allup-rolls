54c3c1da8d3b7fbfdb1867677b8945677e77444c
faridrma <faridrma@code.edu.az>
2023-03-16 11:17:32 +0400

    rol

---
+++ back-main/BigBackEnd/BigBackEnd/Areas/Manage/ViewModels/AccountViewModels/ChangeRoleVM.cs
@@ -0,0 +1,9 @@
+﻿namespace BigBackEnd.Areas.Manage.ViewModels.AccountViewModels
+{
+    public class ChangeRoleVM
+    {
+        public string Id { get; set; }
+        public string RoleId { get; set; }
+
+    }
+}
